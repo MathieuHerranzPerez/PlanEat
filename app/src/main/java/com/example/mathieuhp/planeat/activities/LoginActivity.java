d3c0e5a1bf31bdff00974d9284350697c70f3de0
MathieuHerranzPerez <mathieu.herranzperez@gmail.com>
2018-11-08 10:29:36 -0600

    SignUp possiblity

--- app/src/main/java/com/example/mathieuhp/planeat/activities/LoginActivity.java
+++ app/src/main/java/com/example/mathieuhp/planeat/activities/LoginActivity.java
@@ -1,10 +1,19 @@
 package com.example.mathieuhp.planeat.activities;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.example.mathieuhp.planeat.R;
 import com.facebook.CallbackManager;
@@ -14,13 +23,26 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
-    LoginButton loginButton;
+    Toolbar toolbar;
+    ProgressBar progressBar;
+    EditText emailSignUp;
+    EditText passwordSignUp;
+    Button signUpBtn;
+
+    private FirebaseAuth firebaseAuth;
+
+    LoginButton loginButtonFacebook;
     TextView textView;
     CallbackManager callbackManager;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +51,48 @@ public class LoginActivity extends AppCompatActivity {
         AppEventsLogger.activateApp(this);
 
         setContentView(R.layout.activity_login);
-        loginButton = (LoginButton) findViewById(R.id.facebook_login_button);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar_login);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        emailSignUp = (EditText) findViewById(R.id.signup_email);
+        passwordSignUp = (EditText) findViewById(R.id.signup_password);
+        signUpBtn = (Button) findViewById(R.id.btn_signup);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                firebaseAuth.createUserWithEmailAndPassword(emailSignUp.getText().toString(),
+                        passwordSignUp.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                       @Override
+                       public void onComplete(@NonNull Task<AuthResult> task) {
+                           progressBar.setVisibility(View.GONE);
+                            if(task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, getResources().getString(R.string.registered),
+                                        Toast.LENGTH_LONG).show();
+                                emailSignUp.setText("");
+                                passwordSignUp.setText("");
+                            }
+                            else {
+                                Toast.makeText(LoginActivity.this, task.getException().getMessage(),
+                                        Toast.LENGTH_LONG).show();
+                            }
+                       }
+                   }
+                );
+            }
+        });
+
+
+        /* --- FACEBOOK --- */
+
+        loginButtonFacebook = (LoginButton) findViewById(R.id.facebook_login_button);
         textView = (TextView) findViewById(R.id.login_status);
         callbackManager = CallbackManager.Factory.create();
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        loginButtonFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // TODO
